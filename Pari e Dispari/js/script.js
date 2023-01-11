d5bc7fa75feabbba414b1940c39f3fe09e5a4dd8
Iacopo Buffoni <itsmadpupp@outlook.it>
2023-01-11 08:42:18 +0100

    Finito

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,13 +1,41 @@
-let _SceltaTipo = prompt("Iniziamo ! Scegli Pari o Dispari ?");
+let _SceltaTipo = prompt("Iniziamo ! Scegli Pari o Dispari ?").toLowerCase();
 let _SceltaNum = prompt("Adesso scegli un numero da 1 a 5 !");
 _SceltaNum = parseInt(_SceltaNum);
-let _Random;
+let _PariDispari;
 
-function RandomNumber(random) {
+function RandomNumber (min, max) {
 
-    _Random = Math.floor(Math.random() (5 - 1)) + 1;
-    return _Random;
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+
 }
 
-let _SceltaPc = RandomNumber(_Random);
-console.log(_SceltaPc);
+function PariDispari (num) {
+
+    if (num % 2 == 0) {
+
+        alert("La somma dei numeri è " + _Somma + " , PARI !");
+        _PariDispari = "Pari".toLowerCase();
+
+    } else {
+
+        alert("La somma dei numeri è " + _Somma + " , DISPARI !");
+        _PariDispari = "Dispari".toLowerCase();
+
+    }
+
+    return _PariDispari;
+}
+
+let _SceltaPc = RandomNumber(1, 5);
+let _Somma = _SceltaNum + _SceltaPc;
+let _Risultato = PariDispari(_Somma);
+
+if (_Risultato == _SceltaTipo) {
+
+    alert("Complimenti ! Hai vinto tu !");
+
+} else {
+
+    alert("Mi dispiace ! Ha vinto il Pc !");
+
+}
